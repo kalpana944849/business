c0be01c5e54d0d110e98ca51a65ddcea963d8602
Ayush Suyal <ayushsuyal@Ayushs-MacBook-Air.local>
2023-03-01 11:02:51 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import Index from './view';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+     home
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Navbar =()=>{
+    return(
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
+export default Navbar;
